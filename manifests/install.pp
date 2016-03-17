40431e90d7b46b5fa2586c8e0faefb0e3d96e057
Daniel Hopper <daniel.hopper@cdk.com>
2016-03-17 10:46:41 -0400

    fixup! Updating ui download to use archive

--- manifests/install.pp
+++ manifests/install.pp
@@ -34,52 +34,43 @@ class consul::install {
         extract       => true,
         extract_path  => "${install_path}/consul-${consul::version}",
         creates       => "${install_path}/consul-${consul::version}/consul",
+      }->
+      file {
+        "${install_path}/consul-${consul::version}/consul":
+          owner => 'root',
+          group => 0, # 0 instead of root because OS X uses "wheel".
+          mode  => '0555';
+        "${consul::bin_dir}/consul":
+          ensure => link,
+          notify => $consul::notify_service,
+          target => "${install_path}/consul-${consul::version}/consul";
       }
-
-      #file { "${::staging::path}/consul-${consul::version}":
-      #  ensure => directory,
-      #}
-      #staging::extract { "consul-${consul::version}.${consul::download_extension}":
-      #  target  => "${::staging::path}/consul-${consul::version}",
-      #  creates => "${::staging::path}/consul-${consul::version}/consul",
-      #} ->
-      #file {
-      #  "${::staging::path}/consul-${consul::version}/consul":
-      #    owner => 'root',
-      #    group => 0, # 0 instead of root because OS X uses "wheel".
-      #    mode  => '0555';
-      #  "${consul::bin_dir}/consul":
-      #    ensure => link,
-      #    notify => $consul::notify_service,
-      #    target => "${::staging::path}/consul-${consul::version}/consul";
-      #}
 
       if ($consul::ui_dir and $consul::data_dir) {
 
         # The 'dist' dir was removed from the web_ui archive in Consul version 0.6.0
         if (versioncmp($::consul::version, '0.6.0') < 0) {
-          $staging_creates = "${consul::data_dir}/${consul::version}_web_ui/dist"
-          $ui_symlink_target = $staging_creates
+          $archive_creates = "${consul::data_dir}/${consul::version}_web_ui/dist"
+          $ui_symlink_target = $archive_creates
         } else {
-          $staging_creates = "${consul::data_dir}/${consul::version}_web_ui/index.html"
+          $archive_creates = "${consul::data_dir}/${consul::version}_web_ui/index.html"
           $ui_symlink_target = "${consul::data_dir}/${consul::version}_web_ui"
         }
 
-        #file { "${consul::data_dir}/${consul::version}_web_ui":
-        #  ensure => 'directory',
-        #  owner  => 'root',
-        #  group  => 0, # 0 instead of root because OS X uses "wheel".
-        #  mode   => '0755',
-        #} ->
-        #staging::deploy { "consul_web_ui-${consul::version}.zip":
-        #  source  => $consul::real_ui_download_url,
-        #  target  => "${consul::data_dir}/${consul::version}_web_ui",
-        #  creates => $staging_creates,
-        #} ->
-        #file { $consul::ui_dir:
-        #  ensure => 'symlink',
-        #  target => $ui_symlink_target,
-        #}
+        file { "${install_path}/consul-${consul::version}_web_ui":
+          ensure => directory,
+        }->
+        archive { "${install_path}/consul_web_ui-${consul::version}.zip":
+          ensure        => present,
+          source        => $consul::real_ui_download_url,
+          extract       => true,
+          extract_path  => "${install_path}/consul-${consul::version}_web_ui",
+          creates       => $archive_creates,
+        }->
+        file { $consul::ui_dir:
+          ensure => 'symlink',
+          target => $ui_symlink_target,
+        }
       }
     }
     'package': {
